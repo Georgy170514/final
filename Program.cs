00874549f4beb39a98db8774cbe99ff326ebe341
Georgy Loginov <georgylog@gmail.com>
2024-02-24 23:48:33 +0300

    first part

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество элементов в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
+        
+        string[] inputArray = new string[n];
+
+       
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите значение {i + 1}: ");
+            inputArray[i] = Console.ReadLine();
+        }
+
+        
+        string[] resultArray = FilterStrings(inputArray);
+
+
